80b2798f00bb5480af3fc516fe01caf7556123d7
Ed <edward.vercoe@gmail.com>
2021-12-02 11:29:51 +1100

    contentful integration

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import "css/index.css";
 import "css/PreviewBar.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Layout from "./Layout";
+import Layout from "../helpers/Layout";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,35 @@ import AnimationRevealPage from "../helpers/AnimationRevealPage.js";
 import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
 import Testimonial from "components/testimonials/TwoColumnWithImage.js";
 import ContactUsForm from "components/forms/TwoColContactUsWithIllustrationFullForm.js";
-import RecentWork from "components/features/VerticalWithAlternateImageAndText.js";
+import RecentWork from "components/features/RecentWork.js";
 import Footer from "components/footers/MiniCenteredFooter.js";
 import Team from "components/cards/ProfileThreeColGrid";
 
-export default function Home() {
+import { fetchEntries } from "../contentful/recentWork";
+import safeJsonStringify from "safe-json-stringify";
+
+export default function Home({ posts }) {
   return (
     <AnimationRevealPage disabled={true}>
       <Hero />
-      <RecentWork />
+      <RecentWork posts={posts} />
       <Testimonial />
       <Team />
       <ContactUsForm />
       <Footer />
     </AnimationRevealPage>
   );
+}
+
+export async function getStaticProps() {
+  const rawData = await fetchEntries({ content_type: "homeHighlightWork" });
+  const stringifiedData = safeJsonStringify(rawData);
+  const posts = JSON.parse(stringifiedData);
+
+  return {
+    props: {
+      posts: posts[0].fields,
+    },
+    revalidate: 5,
+  };
 }
